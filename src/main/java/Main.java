e3cd4e8960f3acd6a969fefdfb4e87ac50ed5aaa
rachelstans <101438461+rachelstans@users.noreply.github.com>
2023-03-04 21:21:59 -0700

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,31 @@
-public class Main {
+ public class Main {
+         public static void main(String[] args) {
+
+                 System.out.println("Welcome to the calculator");
+                 System.out.println("Enter a command:");
+
+                 if (args[0] == "add") {
+                        System.out.println("add");
+                 }
+
+                 if (args[0] == "subtract") {
+                         System.out.println("subtract");
+                 }
+
+                 if (args[0] == "multiply") {
+                         System.out.println("multiply");
+                 }
+
+                 if (args[0] == "divide") {
+                         System.out.println("divide");
+                 }
+
+                 if (args[0] == "fibonacci") {
+                         System.out.println("fibonacci");
+                 }
+
+                 if (args[0] == "binary") {
+                         System.out.println("binary");
+                 }
+         }
 }
